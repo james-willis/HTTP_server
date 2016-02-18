3da1856580872dba9441e956004c5bfaf7f384e5
James Willis <jameswillis2017@u.northwestern.edu>
2016-02-18 06:48:45 -0600

    Implemented Channels
    *implemented use of channels instead of mutexes and arcs
    *Implemented log writing thread
    *implemented valid request checking
    * started implementing handle_client

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
 use std::fs::File;
 use std::net::{TcpListener, TcpStream};
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{channel, sync_channel, Sender, Receiver};
 use std::thread;
+use std::io::{Read, Write};
+use chrono::*;
 extern crate chrono;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    let log = File::create("log.txt").unwrap();
-    let log_mut = Arc::new(Mutex::new(log));
+    let mut log_file = File::create("log.txt").unwrap();
+    let (log, receiver) = channel::<String>();
+
+    //spin up logging thread
+    thread::spawn(move|| {
+        loop {
+            let msg = receiver.recv().unwrap();
+            log_file.write(&*msg.into_bytes());
+            log_file.write("\n".as_bytes());
+        }
+    });
+
+    //spin up listener threads
     for stream in listener.incoming() {
-        let log_mut = log_mut.clone();
+        let log = log.clone();
         match stream {
             Ok(stream) => {
                 thread::spawn(move|| {
-                    // connection succeeded
-                    unimplemented!();
-                    handle_client(stream);
+                    handle_client(stream, log);
                 });
             },
-            Err(e) => write_log(log_mut, "Failed Connection"),
+            Err(e) => {log.send("Failed Connection Attempt".to_string());},
         }
     }
 
@@ -26,18 +37,30 @@ fn main() {
     drop(listener);
 }
 
-fn handle_client(stream: TcpStream) {
+fn handle_client(mut stream: TcpStream, rec: Sender<String>) {
     //determines status of given request,
-    //spins up a new thread and writes to
-    //the log
-    unimplemented!();
+    //serves the appropriate request, and writes to log
+    let time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+    let mut buf = String::new();
+    stream.read_to_string(&mut buf);
+    if is_valid_request(&buf) {
+        //determine if file exists
+            //if exists determine access
+                //if access (200)
+                //else 403
+            //else 404
+    }
+    else {
+        stream.write(&"400 Bad Request: Badly Formatted HTTP request".as_bytes());
+        // pretty damn sure this is wrong
+    }
 }
 
-fn write_log<S: Into<String>>(log: Arc<Mutex<File>>, entry: S) {
-    // prints timestamp and request info to the log
-    // acquire a lock on the file, and writes S
-    let entry = entry.into();
-    unimplemented!();
-    //I added a crate for timestamping, docs here:
-    //https://lifthrasiir.github.io/rust-chrono/chrono/
+fn is_valid_request(request: &String) -> bool {
+    let mut parsed: Vec<&str> = request.split_whitespace().collect();
+    if parsed.len() != 3 {return false;}
+    if parsed.pop().unwrap() != "HTTP" {return false;}
+    parsed.pop();
+    if parsed.pop().unwrap() != "GET" {return false;}
+    true
 }
